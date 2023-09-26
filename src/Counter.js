2ed8d9b4d4253c0ac65b1bd87f9c4ba822afc039
{Ai <{higa0004@algonquinlive.com}>
2023-09-25 22:12:26 -0400

    Added strings to the Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -6,7 +6,11 @@ function Counter(props) {
   
   return (
     <div style={{ padding: '20px' }}>
+      <h2>State example</h2>
+      <p>Incrementing Counter</p>
       <button onClick={() => setCount(count + 1)}>Click me: {count}</button>
+      <h2>Reusable Components example</h2>
+      <p>Incrementing counter transforms to Like Button</p>
       <LikeButton />
     </div>
   );
